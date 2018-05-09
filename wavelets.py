111fa71453ed171b600c0d97b202dc78ad3156b6
MProx <34684113+MProx@users.noreply.github.com>
2018-05-09 13:43:26 +0200

    Create wavelets.py

---
+++ wavelets.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import pywt
+import sys
+
+# Get data:
+file_object  = open('./Biosignals.txt', 'r')
+raw_data = file_object.readlines()
+index = []
+data = []
+for i in range(len(raw_data)-1):
+    x, y = raw_data[i].split('\t')
+    X = float(x)
+    Y = float(y)
+    index.append(X)
+    data.append(Y)
+
+# Create wavelet object and define parameters
+w = pywt.Wavelet('sym4')
+maxlev = pywt.dwt_max_level(len(data), w.dec_len)
+# maxlev = 2 # Override if desired
+print("maximum level is " + str(maxlev))
+threshold = 0.04 # Threshold for filtering
+
+# Decompose into wavelet components, to the level selected:
+coeffs = pywt.wavedec(data, 'sym4', level=maxlev)
+
+#cA = pywt.threshold(cA, threshold*max(cA))
+plt.figure()
+for i in range(1, len(coeffs)):
+    plt.subplot(maxlev, 1, i)
+    plt.plot(coeffs[i])
+    coeffs[i] = pywt.threshold(coeffs[i], threshold*max(coeffs[i]))
+    plt.plot(coeffs[i])
+
+
+datarec = pywt.waverec(coeffs, 'sym4')
+
+
+mintime = 1000
+maxtime = mintime + 2000
+
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.plot(index[mintime:maxtime], data[mintime:maxtime])
+plt.xlabel('time (s)')
+plt.ylabel('microvolts (uV)')
+plt.title("Raw signal")
+plt.subplot(2, 1, 2)
+plt.plot(index[mintime:maxtime], datarec[mintime:maxtime])
+plt.xlabel('time (s)')
+plt.ylabel('microvolts (uV)')
+plt.title("De-noised signal using wavelet techniques")
+
+plt.tight_layout()
+plt.show()
+
+# fs = 500
+# f, t, Sxx = signal.spectrogram(data, fs)
+# plt.pcolormesh(t, f, Sxx)
+# plt.ylabel('Frequency [Hz]')
+# plt.xlabel('Time [sec]')
+# plt.ylim(0, 50)
+# plt.show()
+
